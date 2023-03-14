e08065dc601a9196f150dfe0294b460c4ec14876
FunkyMuse <kikoski@protonmail.com>
2023-03-14 16:41:10 +0100

    add debug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.3.2"
+        kotlinCompilerExtensionVersion = "1.4.3"
     }
     packagingOptions {
         resources {
@@ -47,24 +47,16 @@ android {
 }
 
 dependencies {
-    val accompanist = "0.29.2-rc"
     val androidx_navigation = "2.6.0-alpha07"
-    implementation("com.google.accompanist:accompanist-navigation-animation:$accompanist")
-    implementation("com.google.accompanist:accompanist-navigation-material:$accompanist")
     implementation("androidx.navigation:navigation-compose:$androidx_navigation")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.0")
     implementation("androidx.activity:activity-compose:1.6.1")
-    implementation(platform("androidx.compose:compose-bom:2022.10.00"))
+    implementation(platform("androidx.compose:compose-bom:2023.01.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2022.10.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
--- app/src/main/java/com/example/navigationbug/MainActivity.kt
+++ app/src/main/java/com/example/navigationbug/MainActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -29,25 +25,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.navigation.NavBackStackEntry
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import androidx.navigation.compose.rememberNavController
 import com.example.navigationbug.ui.theme.NavigationBugTheme
-import com.google.accompanist.navigation.animation.AnimatedNavHost
-import com.google.accompanist.navigation.animation.composable
-import com.google.accompanist.navigation.animation.navigation
-import com.google.accompanist.navigation.animation.rememberAnimatedNavController
-import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
-import com.google.accompanist.navigation.material.ModalBottomSheetLayout
-import com.google.accompanist.navigation.material.rememberBottomSheetNavigator
 
-@OptIn(ExperimentalMaterialNavigationApi::class)
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class)
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NavigationBugTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    val bottomSheetNavigator = rememberBottomSheetNavigator()
-                    val navController = rememberAnimatedNavController(bottomSheetNavigator)
+                    val navController = rememberNavController()
                     val bottomBarEntries = remember { setOf("home", "recent") }
                     val navBackStackEntry: NavBackStackEntry? by navController.currentBackStackEntryFlow.collectAsState(null)
                     val route by remember {
@@ -59,68 +50,62 @@ class MainActivity : ComponentActivity() {
                             "Current destination ${navBackStackEntry?.destination.toString()} with arguments ${navBackStackEntry?.arguments.toString()}"
                         )
                     }
-                    ModalBottomSheetLayout(
-                        bottomSheetNavigator = bottomSheetNavigator,
-                    ) {
-                        Scaffold(modifier = Modifier.fillMaxSize(),
-                            bottomBar = {
-                                BottomAppBar {
-                                    bottomBarEntries.forEach { bottomEntry ->
-                                        NavigationBarItem(
-                                            selected = navBackStackEntry?.destination?.route == bottomEntry,
-                                            alwaysShowLabel = true,
-                                            onClick = {
-                                                navController.navigate(bottomEntry) {
-                                                    restoreState = true
-                                                    navController.graph.startDestinationRoute?.let {
-                                                        popUpTo(it) {
-                                                            saveState = true
-                                                        }
+                    Scaffold(modifier = Modifier.fillMaxSize(),
+                        bottomBar = {
+                            BottomAppBar {
+                                bottomBarEntries.forEach { bottomEntry ->
+                                    NavigationBarItem(
+                                        selected = navBackStackEntry?.destination?.route == bottomEntry,
+                                        alwaysShowLabel = true,
+                                        onClick = {
+                                            navController.navigate(bottomEntry) {
+                                                restoreState = true
+                                                navController.graph.startDestinationRoute?.let {
+                                                    popUpTo(it) {
+                                                        saveState = true
                                                     }
-                                                    launchSingleTop = true
                                                 }
-                                            },
-                                            label = {
-                                                Text(
-                                                    modifier = Modifier.wrapContentSize(unbounded = true),
-                                                    softWrap = false,
-                                                    maxLines = 1,
-                                                    textAlign = TextAlign.Center,
-                                                    text = bottomEntry
-                                                )
-                                            },
-                                            icon = {
-
+                                                launchSingleTop = true
                                             }
-                                        )
-                                    }
-                                }
+                                        },
+                                        label = {
+                                            Text(
+                                                modifier = Modifier.wrapContentSize(unbounded = true),
+                                                softWrap = false,
+                                                maxLines = 1,
+                                                textAlign = TextAlign.Center,
+                                                text = bottomEntry
+                                            )
+                                        },
+                                        icon = {
 
-                            }) { paddingValues ->
-                            AnimatedNavHost(
-                                modifier = Modifier.padding(paddingValues),
-                                navController = navController,
-                                startDestination = "home_graph",
-                                enterTransition = { fadeIn(animationSpec = tween(0)) },
-                                exitTransition = { fadeOut(animationSpec = tween(0)) },
-                            ) {
-                                navigation("home", "home_graph") {
-                                    composable("home") {
-                                        Box(
-                                            modifier = Modifier
-                                                .fillMaxSize()
-                                                .background(Color.Red)
-                                        )
-                                    }
+                                        }
+                                    )
                                 }
-                                navigation("recent", "recent_graph") {
-                                    composable("recent") {
-                                        Box(
-                                            modifier = Modifier
-                                                .fillMaxSize()
-                                                .background(Color.Yellow)
-                                        )
-                                    }
+                            }
+
+                        }) { paddingValues ->
+                        NavHost(
+                            modifier = Modifier.padding(paddingValues),
+                            navController = navController,
+                            startDestination = "home_graph",
+                        ) {
+                            navigation("home", "home_graph") {
+                                composable("home") {
+                                    Box(
+                                        modifier = Modifier
+                                            .fillMaxSize()
+                                            .background(Color.Red)
+                                    )
+                                }
+                            }
+                            navigation("recent", "recent_graph") {
+                                composable("recent") {
+                                    Box(
+                                        modifier = Modifier
+                                            .fillMaxSize()
+                                            .background(Color.Yellow)
+                                    )
                                 }
                             }
                         }
